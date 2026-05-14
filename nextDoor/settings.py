4022f4b183c1cc6f315dca9458a7b4e5a53b7dcb
First-git <rajpootvikash7351@gmail.com>
2026-05-14 18:47:43 +0530

    debug false

--- nextDoor/settings.py
+++ nextDoor/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_6jeh16qnz3@b@0bej0wbapf*6gr($ygfarp!yn$k0jc0-whn1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
 # ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
